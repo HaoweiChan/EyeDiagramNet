38de7352fc3266da0e0730028cc4a6def09cf8c3
Haowei Chan <hwchan42@gmail.com>
2025-08-01 22:03:30 +0800

    fix: update validation script to adjust command execution and parameters - Modify the validation script to set a new command for examining pickle data, changing the directory and increasing the maximum files processed, enhancing the script's functionality and efficiency.

--- scripts/linux/validate_data.sh
+++ scripts/linux/validate_data.sh
@@ -2,6 +2,9 @@
 module load Python3/3.12.2_gpu_torch270
 source /proj/siaiadm/ew_predictor/.venv/sipi_torch270/bin/activate.csh
 
-eval python3 -m tests.examine_pickle_data \
-    --pickle_dir /proj/siaiadm/ew_predictor/data/add_ind_dir_cached \
-    --max_files 10 --max_samples 2 $argv
+set python_cmd = ( \
+    python3 -m tests.examine_pickle_data \
+    --pickle_dir /proj/siaiadm/ew_predictor/data/add_ind_dir \
+    --max_files 20 --max_samples 2 $argv \
+)
+bsub -J LongJob -q ML_CPU -app ML_CPU -P d_09017 $python_cmd
